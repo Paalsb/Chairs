37fad8700ae73655082d48c3e683b4c3eb2f9d66
Paalsb <72149187+Paalsb@users.noreply.github.com>
2021-02-09 15:38:05 +0100

    Update chairs_per_person.py

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -2,5 +2,12 @@ f = open("reservations.csv")
 
 for reservation in f:
     name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
+    try:
+    chairs_per_person = 50 // int(number)
+    except ValueError:
+        print("The number should be an integer and not a number")
+
+    except ZeroDivisionError:
+        print("You cannot reserve 0 chairs. dumbdumb")
+        
     print("{} will get {} chairs per person".format(name, chairs_per_person))
